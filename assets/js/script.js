d27a4a9d4fb0b915f21e6dbe6f196a746a160871
AnotherDebug <antonio.faddanno@gmail.com>
2023-09-22 15:19:27 +0200

    [ADD]: loop for Each e templates literals

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,36 +18,38 @@
 2. Posso utilizzare un ciclo for of oppure un for Each per iterare gli oggetti;
 3. Salvo in tre variabili immagine, titolo e testo richiamando l'oggetto stesso e tramite la dot notation richiamo la chiave che mi restituirà il valore;
 4. tramite template literals, inserisco la stringa che stampa ad ogni iterazione le immagini e i dati richiesti;
+5. 
 
 */
 
-
+//1. Creo un array di oggetti che contengono immagini ed informazioni;
 const images = [
   {
-      image: 'img/01.webp',
-      title: 'Marvel\'s Spiderman Miles Morale',
-      text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-  }, {
-      image: 'img/02.webp',
-      title: 'Ratchet & Clank: Rift Apart',
-      text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-  }, {
-      image: 'img/03.webp',
-      title: 'Fortnite',
-      text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-  }, {
-      image: 'img/04.webp',
-      title: 'Stray',
-      text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-  }, {
-      image: 'img/05.webp',
-      title: "Marvel's Avengers",
-      text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
-  }
+    image: "img/01.webp",
+    title: "Marvel's Spiderman Miles Morale",
+    text: "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.",
+  },
+  {
+    image: "img/02.webp",
+    title: "Ratchet & Clank: Rift Apart",
+    text: "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.",
+  },
+  {
+    image: "img/03.webp",
+    title: "Fortnite",
+    text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+  },
+  {
+    image: "img/04.webp",
+    title: "Stray",
+    text: "Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city",
+  },
+  {
+    image: "img/05.webp",
+    title: "Marvel's Avengers",
+    text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
+  },
 ];
-
-
-//1. Salvo tutti i percorsi all'interno delle variabili di cui ho bisogno;
 
 const itemsSliderRef = document.querySelector(".items-slider");
 const itemsThumbsRef = document.querySelector(".items-thumbs");
@@ -56,7 +58,6 @@ const buttonRightRef = document.querySelector(".down");
 const itemRef = document.getElementsByClassName("item");
 const thumbRef = document.getElementsByClassName("thumb");
 
-//2. Creo la mia lista di immagini;
 /*
 const images = [
   "assets/img/01.webp",
@@ -67,7 +68,26 @@ const images = [
 ];
 */
 
-//3. Creo il ciclo dove gestisco la collection images
+//2. Posso utilizzare un ciclo for of oppure un for Each per iterare gli oggetti;
+images.forEach((hero) => {
+  console.log(hero);
+  //3. Salvo in tre variabili immagine, titolo e testo richiamando l'oggetto stesso e tramite la dot notation richiamo la chiave che mi restituirà il valore;
+  const img = hero.image;
+  const title = hero.title;
+  const info = hero.text;
+  console.log(img);
+  console.log(title);
+  console.log(info);
+  itemsSliderRef.innerHTML += `
+<img class="item hide" src="assets/${img}" alt="">
+                    <div class="info">
+                        <h2 id="'title">${title}</h2>
+                        <p id="text">${info}</p>
+                    </div>`;
+
+  itemsThumbsRef.innerHTML += `<img src="assets/${img}" class="thumb" alt="">`;
+});
+
 
 /*
 for (let i = 0; i < images.length; i++) {
@@ -77,15 +97,11 @@ for (let i = 0; i < images.length; i++) {
 }
 */
 
-
-//4. Rimuovo dal mio item la classe 'hide' per vsualizzare l'immagine;
 itemRef[0].classList.remove("hide");
 thumbRef[0].classList.add("active");
 
-//5. Creo una variabile counter per gestire la classe 'hide';
 let counter = 0;
 
-//6. Creo l'evento al click per nascondere/mostrare le immagini;
 buttonRightRef.addEventListener("click", function () {
   itemRef[counter].classList.add("hide");
   thumbRef[counter].classList.remove("active");
@@ -94,17 +110,13 @@ buttonRightRef.addEventListener("click", function () {
   itemRef[counter].classList.remove("hide");
   thumbRef[counter].classList.add("active");
 
-   //quando vado avanti rimuovo hide dal bottone di sinistra
-  buttonLeftRef.classList.remove('hide');
+  buttonLeftRef.classList.remove("hide");
 
-
-  // nascondo il bottone di destra una volta terminate le immagini;
   if (counter === images.length - 1) {
     buttonRightRef.classList.add("hide");
   }
 });
 
-//7. Creo l'evento al click per nascondere/mostrare le immagini al contrario, quindi decremento il counter;
 buttonLeftRef.addEventListener("click", function () {
   itemRef[counter].classList.add("hide");
   thumbRef[counter].classList.remove("active");
@@ -113,10 +125,8 @@ buttonLeftRef.addEventListener("click", function () {
   itemRef[counter].classList.remove("hide");
   thumbRef[counter].classList.add("active");
 
-  //quando torno indetro rimuovo hide dal bottone destro 
-  buttonRightRef.classList.remove('hide');
+  buttonRightRef.classList.remove("hide");
 
-  // nascondo il bottone di sinistra quando il contatore ritorna allo 0;
   if (counter === 0) {
     buttonLeftRef.classList.add("hide");
   }
